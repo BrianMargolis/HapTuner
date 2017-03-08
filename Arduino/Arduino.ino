6e5629765961de3fd3d37d42991e37ee99767804
Brian Margolis <Brian.M.Margolis@gmail.com>
2017-03-08 15:30:34 -0600

    Added library use to arduino

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -3,16 +3,38 @@
  *
  * This example code is in the public domain.
  */
-#include <FreqMeasure.h>
 
+#include <array>
+#include <vector>
+
+#include <FreqMeasure.h>
+#include "pitch_utils.h"
+#include "note_name.h"
+#include "tuned_note.h"
+
+std::array<double, 88> pitch_freqs;
+std::array<note_name *, 88> pitch_names;
 void setup() {
   Serial.begin(57600);
   FreqMeasure.begin();
-}
 
+  // pitch_freqs[i] = frequency of the ith tuned_note on a piano (A0 = 0)
+  double reference = 440.0;
+  
+  pitch_freqs = get_pitch_freqs(reference);
+
+  // pitch_names[i] = *(tuned_note object with letter (ABCDEFG), modifier (n#b), and octave number for the ith tuned_note on a piano (A0 = 0))
+  pitch_names = get_pitch_names();
+
+    
+}
 double sum=0;
 int count=0;
 
+
+  std::vector<float> freqs;
+  std::vector<tuned_note> notes;
+  
 void loop() {
   if (FreqMeasure.available()) {
     // average several reading together
@@ -20,7 +42,19 @@ void loop() {
     count = count + 1;
     if (count > 30) {
       float frequency = FreqMeasure.countToFrequency(sum / count);
-      Serial.println(frequency);
+
+      freqs.push_back(frequency);
+
+      // Find the nearest frequency and get that, as well as the distance from
+      tuned_note n = freq_to_note(frequency, pitch_freqs);
+      note_name name = *pitch_names[n.getPitch()];
+      // Append it to the notes array
+      notes.push_back(n);
+      
+      // Some useful print statements for debugging
+      Serial.printf("freq %f", frequency);
+      Serial.printf("closest tuned_note %c%c", name.getName(), name.getModifier());
+      Serial.printf("distance to closest tuned_note %f", n.getDistance()); // note that this ranges from -1 (most flat) to 1 (most sharp).
       sum = 0;
       count = 0;
     }
