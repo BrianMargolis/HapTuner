3b00f141827524de267700d7fb58b34ad8c51fd9
Brian Margolis <Brian.M.Margolis@gmail.com>
2017-03-16 15:15:14 -0500

    Adjusted tolerance, reformatted code

--- HapTuner/HapTuner.ino
+++ HapTuner/HapTuner.ino
@@ -11,11 +11,11 @@
 #include "note_name.h"
 #include "tuned_note.h"
 
-// GUItool: begin automatically generated code
-AudioInputAnalog         adc1;           //xy=179,197
-AudioAnalyzeNoteFrequency notefreq;      //xy=482,345
-AudioConnection          patchCord1(adc1, notefreq);
-// GUItool: end automatically generated code
+
+AudioInputAnalog            adc;         
+AudioAnalyzeNoteFrequency   notefreq;  
+AudioConnection             patchCord1(adc, notefreq);
+
 
 // declare reference arrays that are initialized in setup()
 std::array<float, 88> pitch_freqs;
@@ -40,19 +40,14 @@ void setup() {
   delay(1000); // TODO: adjust!
 }
 
-// accumulated frequency and note arrays
-std::vector<float> freqs;
-std::vector<tuned_note> notes;
-
 float freq, prob;
 int doit = 0;
-double tolerance = .01; // acceptable distance from a note to be "in tune"
+double tolerance = .02; // acceptable distance from a note to be "in tune"
 
 void loop() {
   if (notefreq.available()) {
     freq = notefreq.read();
     prob = notefreq.probability();
-//    Serial.printf("Note: %3.2f | Probability: %.2f\n", freq, prob);
 
     // get the nearest note
     tuned_note n = freq_to_note(freq, pitch_freqs);
@@ -63,22 +58,21 @@ void loop() {
     char name = note.getName();
     char modifier = note.getModifier();
 
-    // some useful print statements for debugging
+    // useful print statements for debugging
     Serial.printf("freq %f\n", freq);
-    Serial.printf("closest tuned_note %c%c\n", note.getName(), note.getModifier());
-    Serial.printf("distance to closest tuned_note %f\n", n.getDistance()); // note that this ranges from -1 (most flat) to 1 (most sharp).
+    Serial.printf("closest tuned_note %c%c\n", name, modifier);
+    Serial.printf("distance to closest tuned_note %f\n\n", distance); // note that this ranges from -1 (most flat) to 1 (most sharp).
 
-//    if (distance < -tolerance) {
-//      digitalWrite(flat, HIGH);
-//      digitalWrite(sharp, LOW);
-//    } else if (distance > tolerance) {
-//      digitalWrite(flat, LOW);
-//      digitalWrite(sharp, HIGH);
-//    } else {
-//      digitalWrite(flat, LOW);
-//      digitalWrite(sharp, LOW);
-//    }
-
+    if (distance < -tolerance) {
+      digitalWrite(flat, HIGH);
+      digitalWrite(sharp, LOW);
+    } else if (distance > tolerance) {
+      digitalWrite(flat, LOW);
+      digitalWrite(sharp, HIGH);
+    } else {
+      digitalWrite(flat, LOW);
+      digitalWrite(sharp, LOW);
+    }
   }
 }
 
